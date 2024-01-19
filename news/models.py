f13e1dd9840d98673ab2583329db41d4ac3ab337
oraz <o673355@mail.ru>
2024-01-19 11:31:01 +0500

    [init]

---
+++ news/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.urls import reverse_lazy
+
+
+class News(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Наименование')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    views = models.IntegerField(default=0)  # При создании новости значение по умл 0 но в дальнейшем ростет как счетчик
+
+    """
+    redirect происходит благодоря методу get_absolute_url
+    который собственно получает url конкретного адреса
+    конкретной модели то есть конкретной новости
+    """
+
+    def get_absolute_url(self):
+        return reverse_lazy('view_news', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']  # (-) минус это обратный порядок вывода данных
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def get_absolute_url(self):  # обратное разрешение адресов
+        return reverse_lazy('category', kwargs={'category_id': self.pk})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
